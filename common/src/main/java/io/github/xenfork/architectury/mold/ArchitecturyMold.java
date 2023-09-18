f8c7246927e95065e749168447d59da371d3c27a
baka4n <qq474899581@gmail.com>
2023-09-18 15:46:14 +0800

    update

---
+++ common/src/main/java/io/github/xenfork/architectury/mold/ArchitecturyMold.java
@@ -0,0 +1,11 @@
+package io.github.xenfork.architectury.mold;
+
+public class ArchitecturyMold
+{
+	public static final String MOD_ID = "architectury_mold";
+
+
+	public static void init() {
+
+	}
+}
